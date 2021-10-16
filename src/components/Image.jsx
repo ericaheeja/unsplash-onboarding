52bf385a56faa800164afcbd5a8f284797816a81
heejajeong <heejaerica@gmail.com>
2021-10-16 19:44:08 +0900

    - 마우스오버시 추가 정보 미완성.

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,8 +2,12 @@ const Image = ({ image, scrollRef }) => {
   const { imageUrl, authorName, authorUserName, thumbnail, id } = image;
   console.log(image);
   return (
-    <div ref={scrollRef}>
+    <div className={"image-container"} ref={scrollRef}>
       <img className={"image"} src={imageUrl} alt={authorUserName} />
+      <div className={"detail"}>
+        <img className={"author-image"} src={thumbnail} />
+        <div className={"author-name"}>{authorUserName}</div>
+      </div>
     </div>
   );
 };
