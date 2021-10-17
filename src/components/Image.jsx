9423a61b7ea9f00b0b77c907a96637ab7aad9115
heejajeong <heejaerica@gmail.com>
2021-10-17 21:45:43 +0900

    - 마우스오버시 추가 정보 기능 완료.

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,11 +2,14 @@ const Image = ({ image, scrollRef }) => {
   const { imageUrl, authorName, authorUserName, thumbnail, id } = image;
   console.log(image);
   return (
-    <div className={"image-container"} ref={scrollRef}>
-      <img className={"image"} src={imageUrl} alt={authorUserName} />
-      <div className={"detail"}>
-        <img className={"author-image"} src={thumbnail} />
-        <div className={"author-name"}>{authorUserName}</div>
+    <div className={"image-detail-container"} ref={scrollRef}>
+      <div className={"image-container"}>
+        <img className={"image"} src={imageUrl} alt={authorUserName} />
+
+        <div className={"detail"}>
+          <img className={"author-image"} src={thumbnail} />
+          <div className={"author-name"}>{authorName}</div>
+        </div>
       </div>
     </div>
   );
